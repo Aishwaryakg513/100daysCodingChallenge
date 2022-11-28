b5ff33286520c815d1edff4b00a36718d333861a
Aishwarya KG <69080579+Aishwaryakg513@users.noreply.github.com>
2022-11-28 13:41:09 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,21 @@
+//program to return the ascii value
+/*Description: Get an input character from the user and give the ASCII value of the given input as the output.
+
+Input: b
+
+Output: 98
+
+Input: B
+
+Output: 66*/
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    char c = sc.next().charAt(0);
+	    
+	    System.out.printf("%d",(int)c);
+	}
+}
